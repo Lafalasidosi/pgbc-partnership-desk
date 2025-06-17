4ae08239d61d04cc826ba35a9d2e05c5d3843a5e
Lafalasidosi <lafalasidosi@pm.me>
2025-06-17 13:56:32 -0700

    draft database

--- lib/partnership_desk.dart
+++ lib/partnership_desk.dart
@@ -1,21 +1,33 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'player.dart';
 
 // import 'guest_book_message.dart';
 import 'src/widgets.dart';
 
 class PartnershipDesk extends StatefulWidget {
-  const PartnershipDesk({super.key, required this.registerForPartner, required this.deregisterForPartner});
+  PartnershipDesk({super.key, required this.registerForPartner, required this.deregisterForPartner});
 
   final FutureOr<void> Function() registerForPartner;
   final FutureOr<void> Function() deregisterForPartner;
+  final List<Player> players = [];
 
   @override
   State<StatefulWidget> createState() => _PartnershipDeskState();
 }
 
-class _PartnershipDeskState {
+class _PartnershipDeskState extends State<PartnershipDesk>{
   final _formKey = GlobalKey<FormState>(debugLabel: '_PartnershipDeskState');
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        for (var p in widget.players)
+          ElevatedButton(onPressed: () {}, child: Text(p.getName()))
+      ],
+    );
+  }
 }
 
