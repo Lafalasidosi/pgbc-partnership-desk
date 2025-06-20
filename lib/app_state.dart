eb4bf2ab8e67089fd89fb05a389cff755f65f26d
Lafalasidosi <lafalasidosi@pm.me>
2025-06-19 17:20:50 -0700

    format document per vscode

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -2,18 +2,15 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'
-  hide EmailAuthProvider, PhoneAuthProvider;
+    hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pdv0/player.dart';
 
 import 'firebase_options.dart';
-import 'player.dart';
-
 
 class ApplicationState extends ChangeNotifier {
-
   // Constructor
   ApplicationState() {
     init();
@@ -25,29 +22,35 @@ class ApplicationState extends ChangeNotifier {
   StreamSubscription<QuerySnapshot>? _partnershipDeskSubscription;
   List<Player> _players = [];
   List<Player> get players => _players;
-
+  var currentDate = DateTime.now();
 
   // Funtions
   Future<void> init() async {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-    FirebaseUIAuth.configureProviders([
-      EmailAuthProvider(),
-    ]);
+    FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
 
-// There's a lot going on here, so you should explore it in a debugger to inspect what happens to get a clearer mental model. 
+    // There's a lot going on here, so you should explore it in a debugger to inspect what happens to get a clearer mental model.
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
         _loggedIn = true;
         _partnershipDeskSubscription = FirebaseFirestore.instance
-          .collection('partnershipdesk')
-          .snapshots()
-          .listen((snapshot) {
-            _players = []; 
-            for (final document in snapshot.docs) {
-              _players.add(Player(name: FirebaseAuth.instance.currentUser!.displayName as String));
-            }
-          });
+            .collection('partnershipdesk')
+            .snapshots()
+            .listen((snapshot) {
+              _players = [];
+              for (final document in snapshot.docs) {
+                _players.add(
+                  Player(
+                    name:
+                        FirebaseAuth.instance.currentUser!.displayName
+                            as String,
+                  ),
+                );
+              }
+            });
         notifyListeners();
       } else {
         _loggedIn = false;
@@ -58,17 +61,19 @@ class ApplicationState extends ChangeNotifier {
     }); // FirebaseAuth
   } // Future<void>
 
-Future<DocumentReference> addPlayerToPartnershipDesk() {
+  Future<DocumentReference> addPlayerToPartnershipDesk() {
     if (!_loggedIn) {
       throw Exception('You must be logged in to do that!');
     }
 
     return FirebaseFirestore.instance
-    .collection('partnershipdesk')
-    .add(<String, dynamic>{
-      'username': FirebaseAuth.instance.currentUser!.displayName,
-      'timestamp': DateTime.now().millisecondsSinceEpoch,
-      'userID': FirebaseAuth.instance.currentUser!.uid,
-    }); // FirebaseFirestore
-  } 
+        .collection('partnershipdesk')
+        .add(<String, dynamic>{
+          'username': FirebaseAuth.instance.currentUser!.displayName,
+          'timestamp': DateTime.now().millisecondsSinceEpoch,
+          'userID': FirebaseAuth.instance.currentUser!.uid,
+        }); // FirebaseFirestore
+  }
+
+  // DateTime next
 }
