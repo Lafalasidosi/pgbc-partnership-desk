f74360711da6cf02b72d2839b1f32c7c5fd28032
Lafalasidosi <lafalasidosi@pm.me>
2025-06-20 17:36:40 -0700

    created registration widget idea is to simplify the app to one screen for now

---
+++ lib/src/registration_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class RegistrationWidget extends StatefulWidget {
+  RegistrationWidget({super.key});
+
+  @override
+  _RegistrationWidgetState createState() => _RegistrationWidgetState();
+}
+
+class _RegistrationWidgetState extends State<RegistrationWidget> {
+  var current_date = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      child: Column(
+        children: [
+          Text(current_date.toString()),
+          RegistrationButton(text: 'I need a partner'),
+          RegistrationButton(text: 'I have a partner'),
+        ],
+      ),
+    );
+  }
+}
+
+class RegistrationButton extends StatelessWidget {
+  final String text;
+  final VoidCallback? function;
+
+  RegistrationButton({required this.text, this.function});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(2),
+      child: ElevatedButton(
+        onPressed: function,
+        style: ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll(Color(0xff5000aa)),
+          foregroundColor: WidgetStatePropertyAll(Color(0xffffffff)),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
