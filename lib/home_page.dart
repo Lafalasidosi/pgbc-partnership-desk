04f2b029943f07ff108c9964d72bdac479da645a
Lafalasidosi <lafalasidosi@pm.me>
2025-06-30 16:40:25 -0700

    refactor: move partnership_desk.dart to src folder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:pdv0/partnership_desk.dart';
+import 'package:pdv0/src/partnership_desk.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
