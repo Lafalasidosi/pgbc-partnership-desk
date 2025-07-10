cd61689f43686629d77cba959bf3b5b025c7c65d
Lafalasidosi <lafalasidosi@pm.me>
2025-07-09 18:43:29 -0700

    replace Placeholder in Request

--- lib/request.dart
+++ lib/request.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Request extends StatelessWidget {
@@ -13,6 +14,25 @@ class Request extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    Widget result;
+    result = Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: Color.fromARGB(0, 230, 168, 52),
+      ),
+      child: Column(
+        children: [
+          Text('$requestor invites you to a game: $gameTime'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text('Accept')),
+              ElevatedButton(onPressed: () {}, child: Text('Decline')),
+            ],
+          ),
+        ],
+      ),
+    );
+    return result;
   }
 }
